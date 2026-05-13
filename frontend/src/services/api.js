4b67c05a42e2b75c74e7d65e11e5e0bc2b3d5333
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2026-05-13 20:17:19 +0530

    fix url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000/api';
-// const API_BASE_URL = 'https://srps-backenndd.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
